ed793ccedd129877966f529357a60bee03616771
koti <krao.netapp@gmail.com>
2024-02-10 16:55:17 +0530

    Hiii hello

---
+++ src/StudentId.java
@@ -0,0 +1,10 @@
+Public class StudentId{
+    private String name;
+    private String fatherName;
+
+
+    public StudentId(String name, String fatherName) {
+        this.name = name;
+        this.fatherName = fatherName;
+    }
+}
